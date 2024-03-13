f12363e34b4ee3849bfa791af6b349fa25d9f345
ishant1800 <ishanaryan45@gmail.com>
2024-03-13 18:02:37 +0530

    updated

--- vite-project/src/components/QuizComponent.jsx
+++ vite-project/src/components/QuizComponent.jsx
@@ -1,87 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import quizData from "./quizQuestion.json";
 
-class QuizComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      x: 0,
-      y: quizData,
-    };
-  }
+const QuizComponent = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-  previous = () => {
-    this.setState((prevState) => ({
-      x: Math.max(prevState.x - 1, 0),
-    }));
+  const previous = () => {
+    setCurrentQuestionIndex((prevIndex) => Math.max(prevIndex - 1, 0));
   };
 
-  next = () => {
-    const { x, y } = this.state;
-    if (x === y.length - 1) {
-      this.setState({ x: 0 });
+  const next = () => {
+    if (currentQuestionIndex === quizData.length - 1) {
+      setCurrentQuestionIndex(0);
     } else {
-      this.setState((prevState) => ({
-        x: prevState.x + 1,
-      }));
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
     }
   };
 
-  quit = () => {
-    const confirmQuit = window.confirm(
-      "Are you sure you want to quit the game?"
-    );
-
+  const quit = () => {
+    const confirmQuit = window.confirm("Are you sure you want to quit the game?");
     if (confirmQuit) {
       alert("You quit the game");
-      // Redirect to the first question upon quitting
-      this.setState({ x: 0 });
+      setCurrentQuestionIndex(0);
     }
   };
 
-  render() {
-    const { x, y } = this.state;
-    const finalQ = x === y.length - 1;
-    let text;
-    if (finalQ) {
-      text = "Reset";
-    } else {
-      text = "Next";
-    }
-    const presQ = y[x];
+  const currentQuestion = quizData[currentQuestionIndex];
+  const isFinalQuestion = currentQuestionIndex === quizData.length - 1;
+  const buttonText = isFinalQuestion ? "Reset" : "Next";
 
-    return (
-      <div id="rect">
-        <div id="ques">
-          <h1>Question</h1>
-
-          <p className="question">{`Question ${x + 1} of ${
-            y.length
-          }`}</p>
-          <p className="text">{presQ.question}</p>
-
-          <div id="choice">
-            <div id="opt1">{presQ.optionA}</div>
-            <div id="opt2">{presQ.optionB}</div>
-            <div id="opt3">{presQ.optionC}</div>
-            <div id="opt4">{presQ.optionD}</div>
-          </div>
-          <div>
-            <button id="previous" onClick={this.previous}>
-              Previous
-            </button>
-            <button id="next" onClick={this.next}>
-              {text}
-            </button>
-            <button id="quit" onClick={this.quit}>
-              Quit
-            </button>
-          </div>
+  return (
+    <div id="rect">
+      <div id="ques">
+        <h1>Question</h1>
+        <p className="question">{`Question ${currentQuestionIndex + 1} of ${quizData.length}`}</p>
+        <p className="text">{currentQuestion.question}</p>
+        <div id="choice">
+          <div id="opt1">{currentQuestion.optionA}</div>
+          <div id="opt2">{currentQuestion.optionB}</div>
+          <div id="opt3">{currentQuestion.optionC}</div>
+          <div id="opt4">{currentQuestion.optionD}</div>
+        </div>
+        <div>
+          <button id="previous" onClick={previous}>Previous</button>
+          <button id="next" onClick={next}>{buttonText}</button>
+          <button id="quit" onClick={quit}>Quit</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default QuizComponent;
